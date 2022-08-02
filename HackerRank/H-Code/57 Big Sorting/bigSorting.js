9bff2f386860370ff374b9cac599f53b6df8e060
Hayk W <hpashinyan@gmail.com>
2022-08-02 13:19:36 -0700

    Aug. 2, 2022

--- HackerRank/H-Code/57 Big Sorting/bigSorting.js
+++ HackerRank/H-Code/57 Big Sorting/bigSorting.js
@@ -3,3 +3,7 @@ function bigSorting(unsorted) {
   unsorted.sort((a, b) => a.length - b.length)
   return unsorted
 }
+//-------------------------------------------
+function bigSorting(unsorted) {
+  return unsorted.sort().sort((a, b) => a.length - b.length)
+}
